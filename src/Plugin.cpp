38a373ec9866b36fe9dbeef66037a91c24468978
Tsptds <152429392+Tsptds@users.noreply.github.com>
2026-07-02 12:27:19 +0300

    Update Plugin.cpp

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -7,6 +7,7 @@
 extern "C" DLLEXPORT bool SKSEPlugin_Load(const SKSE::LoadInterface *skse) {
     LogSetup::initializeLogging();
 
+    // Configure the plugin information in CMakeLists.txt
     INFO("'{}' by '{}' / Skyrim: '{}'", Plugin::Name, Plugin::Author, Plugin::VersionString, REL::Module::get().version().string());
     Init(skse, false);
 
